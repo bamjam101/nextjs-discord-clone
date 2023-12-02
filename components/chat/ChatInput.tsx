11672845e70a1532b6d141c5fa9b04b97a2139ac
bamjam101 <herejamil123@gmail.com>
2023-12-03 01:21:48 +0530

    Created chat input component and state management for the same.

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { ChatInputDataType, chatInputSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
+import { Plus, Smile } from "lucide-react";
+import { Input } from "../ui/input";
 
 type ChatInputProps = {
   apiUrl: string;
@@ -23,7 +28,42 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const onSubmit = async (data: ChatInputDataType) => {
     console.log(data);
   };
-  return <div>ChatInput</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                  >
+                    <Plus className="text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                    {...field}
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                  />
+
+                  <div className="absolute top-7 right-8">
+                    <Smile />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 };
 
 export default ChatInput;
